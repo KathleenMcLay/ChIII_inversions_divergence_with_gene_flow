7fcbc4c00f58d86340edc8eb0954a62dd3a56bc1
KathleenMcLay <mclay.kathleen@gmail.com>
2025-02-03 15:13:51 +1000

    update fst

---
+++ population_pair_divergence/fst.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+module load vcftools
+
+DIR="/QRISdata/Q6656/chapter_III/population_divergence"
+gzvcf="/QRISdata/Q6656/sf8_no_variants_noD1_reheader_final_nsng.vcf.gz"
+
+# Read population file containing pop name, P1 samples, P2 samples
+POP_FILE="/Users/kathleenmclay/ChIII_inversions_divergence_with_gene_flow/population_pair_divergence/pops.txt"
+
+# Loop through each population in the first column of the file
+while IFS=',' read -r pop P1 P2; do
+    # Define output file paths
+    FST_OUT="${DIR}/${pop}_fst"
+    
+    # Create temporary files for sample lists
+    echo "$P1" | tr ";" "\n" > "${DIR}/${pop}_P1.txt"
+    echo "$P2" | tr ";" "\n" > "${DIR}/${pop}_P2.txt"
+
+    # Calculate FST
+    vcftools \
+        --gzvcf ${gzvcf} \
+        --weir-fst-pop "${DIR}/${pop}_P1.txt" \
+        --weir-fst-pop "${DIR}/${pop}_P2.txt" \
+        --fst-window-size 10000 --fst-window-step 10000 \
+        --out ${FST_OUT}
+
+done < "$POP_FILE"
---
+++ population_pair_divergence/temp.py
@@ -0,0 +1,47 @@
+import os
+import glob
+
+# Define directory and output file
+directory = "/QRISdata/Q6656/chapter_III/population_divergence"
+output_file = "/QRISdata/Q6656/chapter_III/population_divergence/dxy_all.txt"
+
+# Find all .txt files in the directory
+file_list = sorted(glob.glob(os.path.join(directory, "*.txt")))
+
+if not file_list:
+    print("No .txt files found in the directory.")
+else:
+    with open(output_file, "w") as outfile:
+        first_file = True
+        for file in file_list:
+            with open(file, "r") as infile:
+                lines = infile.readlines()
+                if first_file:
+                    outfile.writelines(lines)  # Write everything including header
+                    first_file = False
+                else:
+                    outfile.writelines(lines[1:])  # Skip header after the first file
+    print(f"Concatenation complete. Output saved to {output_file}")
+
+
+import pandas as pd
+
+# Define file paths
+csv_file = "/QRISdata/Q6656/chapter_III/population_divergence/LD_all_data.csv"  # Update with your actual CSV file path
+filter_file = "/QRISdata/Q6656/chapter_III/population_divergence/popsLDfil.txt"  # Update with your actual text file path
+output_file = "/QRISdata/Q6656/chapter_III/population_divergence/LD_all.csv"
+
+# Load the filter list from the text file
+with open(filter_file, "r") as f:
+    filter_values = set(line.strip() for line in f if line.strip())
+
+# Load the CSV file
+df = pd.read_csv(csv_file)
+
+# Filter the DataFrame
+filtered_df = df[df['population'].isin(filter_values)]
+
+# Save the filtered DataFrame to a new CSV file
+filtered_df.to_csv(output_file, index=False)
+
+print(f"Filtering complete. Output saved to {output_file}")
